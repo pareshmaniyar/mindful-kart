8e2cb8f4e7d3fcef999149cbf98648a377495e09
pareshmaniyar <pareshmaniyar1@gmail.com>
2021-09-21 13:13:11 +0530

    Add comments

--- AuthService/constants.js
+++ AuthService/constants.js
@@ -3,3 +3,4 @@ const mongodbURL = 'mongodb://localhost/auth-service';
 const REGISTER_URL = '/auth/register';
 const LOGIN_URL = '/auth/login';
 module.exports = { PORT, mongodbURL, REGISTER_URL, LOGIN_URL };
+// TODO: setup babel for commonjs
